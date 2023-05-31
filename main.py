fc46af085471ad4b6b80de2ad5bf12728f96944d
Movie Update <118461390+MovieUpdate@users.noreply.github.com>
2023-06-01 00:05:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -632,6 +632,6 @@ async def callback_handlers(bot: Client, cb: CallbackQuery):
 
          async def stop(self, *args): 
             await super().stop() 
-            self.LOGGER(__name__).info("Bot stopped.")
+            return LOGGER.info("Bot stopped.")
             
             NubBot.run()
