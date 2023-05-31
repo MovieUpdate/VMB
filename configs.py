d666d10c963490a855f1c8ac94b44181d84f19f2
Movie Update <118461390+MovieUpdate@users.noreply.github.com>
2023-05-31 19:58:28 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,7 +4,7 @@ import os
 
 
 class Config(object):
-    API_ID = os.environ.get("API_ID")
+    API_ID = os.environ.get("")
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     SESSION_NAME = os.environ.get("SESSION_NAME", "Video-Merger_Bot")
